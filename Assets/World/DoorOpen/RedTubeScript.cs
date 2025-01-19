e7d02e36f8f769d1aa890a8555edacb2f3a8c0f9
Avra <sergeyfomkin2015@gmail.com>
2025-01-19 18:55:02 +0300

    Change verification method cubes in tubes
    Verification method:
    -Now it's not only one script, it's two scripts for RedTube and GreenTube, which compare tags what cubes are entered;

--- Assets/World/DoorOpen/RedTubeScript.cs
+++ Assets/World/DoorOpen/RedTubeScript.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class RedTubeScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Animator RedCube;
+    private void OnTriggerEnter(Collider other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (other.gameObject.tag == "FirstCube")
+        {
+            Debug.Log("enter");
+            RedCube.SetTrigger("RedCube");
+        }
     }
 }
