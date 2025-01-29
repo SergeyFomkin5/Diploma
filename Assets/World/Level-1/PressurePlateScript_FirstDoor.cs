576dd821ebd68fece04ad63f110727dedd8246db
Сергей Фомкин <144447986+SergeyFomkin5@users.noreply.github.com>
2025-01-29 14:16:39 +0300

    Update PressurePlateScript

--- Assets/World/Level-1/PressurePlateScript_FirstDoor.cs
+++ Assets/World/Level-1/PressurePlateScript_FirstDoor.cs
@@ -4,5 +4,17 @@ using UnityEngine;
 
 public class PressurePlateScript_FirstDoor : MonoBehaviour
 {
-    
+    public int flowerCount = 0;
+    public int cubeOneCount = 0;
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.name == "Flower")
+        {
+            flowerCount = 2;
+        }
+        if (other.gameObject.name == "CubeWithOne")
+        {
+            cubeOneCount = 1;
+        }
+    }
 }
