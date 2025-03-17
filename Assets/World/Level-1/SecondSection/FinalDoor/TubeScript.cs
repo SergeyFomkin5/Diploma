9d5031db80635a4278f8e173f8af9476c1299bd1
Avra <sergeyfomkin2015@gmail.com>
2025-03-17 20:07:43 +0300

    Update TubeScript.cs
    add new messages showing method

--- Assets/World/Level-1/SecondSection/FinalDoor/TubeScript.cs
+++ Assets/World/Level-1/SecondSection/FinalDoor/TubeScript.cs
@@ -5,51 +5,68 @@ using UnityEngine;
 public class TubeScript : MonoBehaviour
 {
     [SerializeField] private Animator gate;
-
     [SerializeField] private GameObject failMessage;
     [SerializeField] private GameObject congratulationsMessage;
+    [SerializeField] public KeyScript key;
 
-    private Coroutine messageCoroutine; // Хранит ссылку на корутину
+    private Coroutine messageCoroutine;
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject == true && other.gameObject.name == "Key")
+        Debug.Log("Объект вошёл в триггер: " + other.gameObject.name);
+        Debug.Log("isKey: " + key.isKey);
+
+        if (key.isKey && other.gameObject.name == "Key")
         {
+            Debug.Log("Условие выполнено: ключ найден");
             gate.SetTrigger("FinalGateOpen");
-            ShowCongratulationslMessage();
-        } else
+            ShowCongratulationsMessage();
+        }
+        else
         {
-            if (messageCoroutine != null)
-            {
-                HideCongratulationsMessage();
-            }
+            Debug.Log("Условие не выполнено: ключ не найден");
+            ShowFailMessage();
         }
     }
 
-    private void ShowCongratulationslMessage()
+    private void ShowCongratulationsMessage()
     {
-        if (messageCoroutine == null)
+        if (!congratulationsMessage.activeSelf)
         {
+            Debug.Log("Показ поздравительного сообщения");
+            HideAllMessages();
             congratulationsMessage.SetActive(true);
-            Debug.Log("Поздравительное сообщение активно");
-            messageCoroutine = StartCoroutine(ShowCongratulationsMessageForSeconds(5f));
+            messageCoroutine = StartCoroutine(HideAfterDelay(5f));
         }
     }
 
-    private void HideCongratulationsMessage()
+    private void ShowFailMessage()
+    {
+        if (!failMessage.activeSelf)
+        {
+            Debug.Log("Показ сообщения об ошибке");
+            HideAllMessages();
+            failMessage.SetActive(true);
+            messageCoroutine = StartCoroutine(HideAfterDelay(5f));
+        }
+    }
+
+    private void HideAllMessages()
     {
         if (messageCoroutine != null)
         {
             StopCoroutine(messageCoroutine);
             messageCoroutine = null;
         }
-
+        failMessage.SetActive(false);
         congratulationsMessage.SetActive(false);
-        Debug.Log("Поздравительное сообщение деактивировано");
     }
 
-    private IEnumerator ShowCongratulationsMessageForSeconds(float seconds)
+    private IEnumerator HideAfterDelay(float delay)
     {
-        yield return new WaitForSeconds(seconds);
-        HideCongratulationsMessage();
+        Debug.Log("Корутина запущена");
+        yield return new WaitForSeconds(delay);
+        Debug.Log("Корутина завершена");
+        HideAllMessages();
     }
 }
