cd968b5f5e18317fd7338e598bc7c2540fef5703
Avra <sergeyfomkin2015@gmail.com>
2025-03-17 19:20:26 +0300

    Update CharTest.cs

--- Assets/World/Level-1/FirstSection/CharTest.cs
+++ Assets/World/Level-1/FirstSection/CharTest.cs
@@ -1,22 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
+// CharTest.cs
 using UnityEngine;
 
 public class CharTest : MonoBehaviour
 {
     [SerializeField] private Animator CharCube;
     public int isCorrectAnswer = 0;
+    public bool isPlaced = false; // Добавляем флаг
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "FourthCube")
+        if (other.CompareTag("FourthCube"))
         {
-            Debug.Log("enter");
             CharCube.SetTrigger("CharCube");
             isCorrectAnswer = 1;
+            isPlaced = true; // Отмечаем размещение
         }
-        else
+        else if (!other.CompareTag("Untagged"))
         {
             isCorrectAnswer = 2;
+            isPlaced = true; // Отмечаем даже при ошибке
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("FourthCube") || other.CompareTag("WrongCube"))
+        {
+            isCorrectAnswer = 0;
+            isPlaced = false; // Сбрасываем флаг
         }
     }
 }
