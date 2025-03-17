e95a63549abddfaeeba46b4a6d1a242561abd1e4
Avra <sergeyfomkin2015@gmail.com>
2025-03-17 20:08:30 +0300

    rename Chech_DoorOne on Check_DoorOne

--- Assets/World/Level-1/SecondSection/FirstDoor/Check_DoorOne.cs
+++ Assets/World/Level-1/SecondSection/FirstDoor/Check_DoorOne.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Check_Door : MonoBehaviour
+public class Check_DoorOne : MonoBehaviour
 {
     [SerializeField] private GameObject firstDoorAnimation;
 
@@ -17,12 +17,12 @@ public class Check_Door : MonoBehaviour
         bool isCubeOnFirstPlate = PressurePlate_FirstDoor.cubeOneCount == 1;
 
         bool isFlowerOnSecondPlate = PressurePlate_FirstDoor_SecondPlate.flowerCount == 2;
-        bool isCubeOnSecondPlate = PressurePlate_FirstDoor_SecondPlate.cubeTwoCount == 1;
+        bool isCubeOnSecondPlate = PressurePlate_FirstDoor_SecondPlate.cubeOneCount == 1;
 
         // Проверяем, находится ли цветок на одной плитке, а куб на другой
         if ((isFlowerOnFirstPlate && isCubeOnSecondPlate) || (isCubeOnFirstPlate && isFlowerOnSecondPlate) ||
-            (isFlowerOnFirstPlate && !isCubeOnFirstPlate && !isCubeOnSecondPlate) ||
-            (isFlowerOnSecondPlate && !isCubeOnFirstPlate && !isCubeOnSecondPlate))
+            (isFlowerOnFirstPlate && !isCubeOnFirstPlate && isCubeOnSecondPlate) ||
+            (isFlowerOnSecondPlate && isCubeOnFirstPlate && !isCubeOnSecondPlate))
         {
             firstDoorAnimation.SetActive(false);
             Debug.Log("Дверь открыта!");
