f01e92474c3ae176c4c36d736f2161c52bff92ef
Avra <sergeyfomkin2015@gmail.com>
2025-04-22 19:40:43 +0300

    Update PauseScript.cs

--- Assets/Player/Scripts/PauseScript.cs
+++ Assets/Player/Scripts/PauseScript.cs
@@ -1,18 +1,71 @@
-using System.Collections;
-using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject pauseMenu;
+    [SerializeField] private GameObject settings;
+    public TerminalList terminalList;
+    public bool isPaused = false;
+    public bool isSettingsActive = false;
+
+    private void Start()
     {
-        
+        pauseMenu.SetActive(false);
+        settings.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+                Resume(); // Если игра на паузе — возобновить
+            else
+                Pause();  // Если игра идет — поставить на паузу
+        }
+
+        if (isSettingsActive && Input.GetKeyDown(KeyCode.Escape))
+        {
+            settings.SetActive(false);
+            isSettingsActive = false;
+        }
     }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        isPaused = false;
+        Time.timeScale = 1.0f;  // Возобновляем время
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Pause()
+    {
+        pauseMenu.SetActive(true);
+        isPaused = true;
+        Time.timeScale = 0f;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1.0f;
+    }
+    public void Settings()
+    {
+        settings.SetActive(true);
+        isSettingsActive = true;
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+
 }
